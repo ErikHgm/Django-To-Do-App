fb777df80f7376e15c7307f0e39980c3cc363ba6
Erik <erikhgm@gmail.com>
2022-09-05 20:28:35 +0000

    Update the todo list template

--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Item(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     done = models.BooleanField(blank=False, null=False, default=False)

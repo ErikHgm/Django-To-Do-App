9e6303a2ac99de157aac9d44e20f21cc912de331
Erik <erikhgm@gmail.com>
2022-09-09 00:19:29 +0000

    Add missing tests using coverage

--- todo/test_models.py
+++ todo/test_models.py
@@ -8,3 +8,7 @@ class TestModels(TestCase):
     def test_done_defaults_to_false(self):
         item = Item.objects.create(name='Test todo item')
         self.assertFalse(item.done)
+
+    def test_Item_stringmethod_returns_name(self):
+        item = Item.objects.create(name='Test todo item')
+        self.assertEqual(str(item), 'Test todo item')
